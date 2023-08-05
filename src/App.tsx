d42f1a00dc95bffae82cfc17b8dff4456eff3bee
jitunayak <jitunayak715@gmail.com>
2023-08-06 03:39:23 +0530

    added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import './App.css'
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomeResults from "./components/HomeResults";
 import StickyHeader from "./components/StickyHeader";
@@ -8,10 +9,17 @@ function App() {
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <StickyHeader />
-        <HomeResults />
-      </QueryClientProvider>
+      <KindeProvider
+        clientId="9f31d731d86b41d4b38dd4988cb9eb42"
+        domain="https://airbnb.kinde.com"
+        logoutUri={window.location.origin}
+        redirectUri={window.location.origin}
+      >
+        <QueryClientProvider client={queryClient}>
+          <StickyHeader />
+          <HomeResults />
+        </QueryClientProvider>
+      </KindeProvider>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { styled } from "@stitches/react";
 import { AirBnbIcon } from "../assets/AirBnbIcon";
 import { Button } from "./Button";
 import { Divider } from "./Divider";
 function Header() {
+  const { login, user, isAuthenticated, isLoading, getPermissions } =
+    useKindeAuth();
+
+  console.log(user);
+  console.log(getPermissions().permissions);
+
   return (
     <HeaderWrapper>
       <AirBnbIcon />
@@ -14,6 +21,9 @@ function Header() {
         <Button color="textSecondary">Add guest</Button>
         <Button color="primary">Search</Button>
       </Button>
+
+      {isAuthenticated && !isLoading && user && <span>{user?.given_name}</span>}
+      {!user && <Button onClick={() => login({})}>Login</Button>}
     </HeaderWrapper>
   );
 }
