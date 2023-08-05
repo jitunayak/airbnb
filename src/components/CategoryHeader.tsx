424d173dba8220e9f4b3e96e23bcdf3b413c336c
jitunayak <jitunayak715@gmail.com>
2023-08-05 14:08:33 +0530

    added categories

--- src/components/CategoryHeader.tsx
+++ src/components/CategoryHeader.tsx
@@ -1,10 +1,7 @@
 import { styled } from "@stitches/react";
-import { useState } from "react";
 
 function CategoryHeader() {
-  const [categories, setCategories] = useState<
-    { url: string; title: string }[]
-  >([
+  const categories: { url: string; title: string }[] = [
     {
       url: "https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
       title: "Farms",
@@ -17,7 +14,23 @@ function CategoryHeader() {
       url: "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
       title: "Amazing Pools",
     },
-  ]);
+    {
+      url: "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+      title: "Domes",
+    },
+    {
+      url: "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+      title: "Rooms",
+    },
+    {
+      url: "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
+      title: "Historical Home",
+    },
+    {
+      url: "https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
+      title: "Coutryside",
+    },
+  ];
   return (
     <Group>
       {categories.map((category) => {
@@ -42,9 +55,12 @@ export const Stack = styled("span", {
   cursor: "pointer",
 });
 export const Group = styled("span", {
-  display: "inline-flex",
+  display: "flex",
   gap: "2rem",
   marginTop: "2rem",
+  justifyContent: "center",
+  alignContent: "center",
+  alignItems: "center",
 });
 export const TitleText = styled("span", {
   fontSize: "13px",
