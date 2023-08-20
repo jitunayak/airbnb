adfe854ecf5d88fadc44720c148a4031c3039b79
jitunayak <jitunayak715@gmail.com>
2023-08-21 01:12:31 +0530

    improved checkbox

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,5 +1,6 @@
-import { styled } from "@stitches/react";
 import React, { useState } from "react";
+
+import { styled } from "../stitches.config";
 interface IProps {
   label: string;
 }
@@ -7,13 +8,17 @@ const CheckBox: React.FC<IProps> = ({ label }) => {
   const [checkBoxValue, setcheckBoxValue] = useState(false);
   return (
     <Group>
-      <input
+      <CheckBoxInput
         checked={checkBoxValue}
         onChange={() => setcheckBoxValue(!checkBoxValue)}
-        style={{ color: "black", height: "1.2rem", width: "1.2rem" }}
         type="checkbox"
       />
-      <div style={{ color: checkBoxValue ? "black" : "gray" }}>{label}</div>
+      <div
+        onClick={() => setcheckBoxValue(!checkBoxValue)}
+        style={{ color: checkBoxValue ? "black" : "gray", cursor: "pointer" }}
+      >
+        {label}
+      </div>
     </Group>
   );
 };
@@ -24,4 +29,10 @@ const Group = styled("div", {
   display: "flex",
   fontSize: "14px",
   gap: "1rem",
+});
+
+const CheckBoxInput = styled("input", {
+  color: "black",
+  height: "1.2rem",
+  width: "1.2rem",
 });
