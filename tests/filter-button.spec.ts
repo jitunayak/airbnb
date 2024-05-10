a58fc9448451e4ad2e67bef0a26dd1840140d288
jitunayak <jitunayak715@gmail.com>
2024-05-10 14:14:28 +0530

    fixes tests

--- tests/filter-button.spec.ts
+++ tests/filter-button.spec.ts
@@ -1,14 +1,14 @@
-import { test } from "@playwright/test";
-import { ai } from "@zerostep/playwright";
+import { expect, test } from "@playwright/test";
+// import { ai } from "@zerostep/playwright";
 
 test("open filters", async ({ page }) => {
   await page.goto("http://localhost:3000");
-  // await page.getByTestId("filter-button").click();
-  // expect(await page.getByTestId("close-filter-modal-button")).toBeVisible();
+  await page.getByTestId("filter-button").click();
+  expect(await page.getByTestId("close-filter-modal-button")).toBeVisible();
 
-  const aiArgs = { page, test };
-  await ai("click on filter button", aiArgs);
-  await ai("validate close button visible", aiArgs);
+  // const aiArgs = { page, test };
+  // await ai("click on filter button", aiArgs);
+  // await ai("validate close button visible", aiArgs);
   // const headerText = await ai("Get the header text", aiArgs);
   // await page.goto("https://google.com/");
   // await ai(`Type "${headerText}" in the search box`, aiArgs);
