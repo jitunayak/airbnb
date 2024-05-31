a4c27be4aa1a27923267b2cea134975fa9cf040e
jitunayak <jitunayak715@gmail.com>
2024-05-31 15:15:10 +0530

    redirect user back to room booking page after successful login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,23 @@
+import { useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+
 import { Footer } from "../components/Footer";
 import HomeResults from "../components/HomeResults";
 import StickyHeader from "../components/StickyHeader";
 
 function Home() {
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const redirectUri = localStorage.getItem('redirectTo');
+    console.log(redirectUri)
+    if (redirectUri) {
+      navigate({ to: redirectUri });
+      localStorage.removeItem('redirectTo');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <StickyHeader />
--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -1,6 +1,6 @@
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { useParams } from '@tanstack/react-router';
+import { useLocation, useParams } from '@tanstack/react-router';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import moment from 'moment';
 import { useState } from 'react';
@@ -14,6 +14,7 @@ import { styled } from '../stitches.config';
 function RoomPage() {
   const { roomsApi } = useApi();
   const { isAuthenticated, login } = useKindeAuth();
+  const { pathname } = useLocation();
   const { roomId } = useParams({ from: '/rooms/$roomId' });
   const {
     data: room,
@@ -67,10 +68,12 @@ function RoomPage() {
 
   const handleReserve = async () => {
     if (!reserveDates.checkIn || !reserveDates.checkOut) return;
-
+    console.log(pathname)
     if (!isAuthenticated) {
+      localStorage.setItem('redirectTo', pathname);
       login();
     }
+
     // sendBookingConfirmation.mutateAsync({
     //   checkInDate: reserveDates.checkIn,
     //   checkOutDate: reserveDates.checkOut,
