070cda2e2b55526f4ab97bf22d137eeac9d5d58a
jitunayak <jitunayak715@gmail.com>
2024-06-06 22:42:49 +0530

    remove tanstack router in production build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,18 @@
 import { QueryClient } from '@tanstack/react-query';
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import React from 'react';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   {
